b12bc49b739c1142e8ca526355e6427979035bb6
fuserwyn <ivansishko@gmail.com>
2022-10-26 11:07:30 +0300

    del folders

--- env/bin/main.py
+++
@@ -1,7 +0,0 @@
-#!/mnt/c/Users/Engineerstallin/Desktop/tsbr/env/bin/python3
-# EASY-INSTALL-DEV-SCRIPT: 'app-example==0.0.1','main.py'
-__requires__ = 'app-example==0.0.1'
-__import__('pkg_resources').require('app-example==0.0.1')
-__file__ = '/mnt/c/Users/Engineerstallin/Desktop/tsbr/app/main.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
