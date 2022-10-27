6d9e09005d2d6fd22b62599ea5a2cfe468b538d4
fuserwyn <ivansishko@gmail.com>
2022-10-27 12:47:38 +0300

    add tests2

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,90 @@ from app.main import app, hello, Deposit
 import json
 from app.main import DataValidationError, _validate_data, valid_open_date
 
+@pytest.mark.parametrize("data, expected_bool", 
+    [({
+        "date": '31.01.2021',
+        "period": 3, 
+        "amount": 10000, 
+        "rate": 6
+    }, True), ({
+        "date": '01.31.2021',
+        "period": 3, 
+        "amount": 10000, 
+        "rate": 6
+    }, False),({
+        "date": '01.31/2021',
+        "period": 3, 
+        "amount": 10000, 
+        "rate": 6
+    }, False), ({
+        "date": 'первое апреля',
+        "period": 3, 
+        "amount": 10000, 
+        "rate": 6
+    }, False)])
+
+def test_valid_open_date(data, expected_bool):
+    assert valid_open_date(data) == expected_bool
+
+@pytest.mark.parametrize("expected_exception, data",
+    [(DataValidationError,{
+        "date": '31.01.2021',
+        "period": 0, 
+        "amount": 10000, 
+        "rate": 6
+    }), (DataValidationError,{
+        "date": '31.01.2021',
+        "period": 3, 
+        "amount": 9999, 
+        "rate": 6
+    }),(DataValidationError,{
+        "date": '31.01.2021',
+        "period": 3, 
+        "amount": 10000, 
+        "rate": 9
+    }),(TypeError,{
+        "date": '31.01.2021',
+        "period": '3', 
+        "amount": 10000, 
+        "rate": 6
+    }),(TypeError,{
+        "date": '31.01.2021',
+        "period": 3, 
+        "amount": '10000', 
+        "rate": 6
+    }),(TypeError,{
+        "date": '31.01.2021',
+        "period": 3, 
+        "amount": 10000, 
+        "rate": '6'
+    })] )
+
+def test_valid_period_or_amount_or_rate_negative(expected_exception, data):
+    with pytest.raises(expected_exception):
+        _validate_data(data)
+
+@pytest.mark.parametrize("data, expected_result",[({
+        "date": '31.01.2021',
+        "period": 3, 
+        "amount": 10000, 
+        "rate": 6
+    }, 200), ({
+        "date": '31.12.2022',
+        "period": 60, 
+        "amount": 3000000, 
+        "rate": 8
+    }, 200),({
+        "date": '19.03.2000',
+        "period": 30, 
+        "amount": 100000, 
+        "rate": 7.99
+    }, 200)
+    ])
+
+def test_valid_period_or_amount_or_rate_positive(data, expected_result):
+    assert _validate_data(data) == expected_result
+
 client = TestClient(app)
         
 def test_main_url():
@@ -31,7 +115,7 @@ def test_post():
                         "31.03.2021": 10100.25,
                         "30.04.2021": 10150.75}
 
-def test_post_bad():
+def test_post_bad_calculation():
     deposit = {
         "date": '31.01.2021',
         "period": 3, 
@@ -92,84 +176,4 @@ def test_post_wrong_rate():
     assert response.status_code == 400
     assert response.json() == {
         "error": "процент по вкладу должен составлять от 1 до 8"}
-
-
-@pytest.mark.parametrize("data, expected_bool", 
-    [({
-        "date": '31.01.2021',
-        "period": 3, 
-        "amount": 10000, 
-        "rate": 6
-    }, True), ({
-        "date": '01.31.2021',
-        "period": 3, 
-        "amount": 10000, 
-        "rate": 6
-    }, False),({
-        "date": '01.31/2021',
-        "period": 3, 
-        "amount": 10000, 
-        "rate": 6
-    }, False), ({
-        "date": 'первое апреля',
-        "period": 3, 
-        "amount": 10000, 
-        "rate": 6
-    }, False)])
-
-def test_valid_data(data, expected_bool):
-    assert valid_open_date(data) == expected_bool
-
-@pytest.mark.parametrize("expected_exception, data",
-    [(DataValidationError,{
-        "date": '31.01.2021',
-        "period": 0, 
-        "amount": 10000, 
-        "rate": 6
-    }), (DataValidationError,{
-        "date": '31.01.2021',
-        "period": 3, 
-        "amount": 9999, 
-        "rate": 6
-    }),(DataValidationError,{
-        "date": '31.01.2021',
-        "period": 3, 
-        "amount": 10000, 
-        "rate": 9
-    }),(TypeError,{
-        "date": '31.01.2021',
-        "period": '3', 
-        "amount": 10000, 
-        "rate": 6
-    }),(TypeError,{
-        "date": '31.01.2021',
-        "period": 3, 
-        "amount": '10000', 
-        "rate": 6
-    })] )
-
-def test_valid_period_or_amount_or_rate_negative(expected_exception, data):
-    with pytest.raises(expected_exception):
-        _validate_data(data)
-
-@pytest.mark.parametrize("data, expected_result",[({
-        "date": '31.01.2021',
-        "period": 3, 
-        "amount": 10000, 
-        "rate": 6
-    }, 200), ({
-        "date": '31.12.2022',
-        "period": 60, 
-        "amount": 3000000, 
-        "rate": 8
-    }, 200),({
-        "date": '19.03.2000',
-        "period": 30, 
-        "amount": 100000, 
-        "rate": 7.99
-    }, 200)
-    ])
-
-def test_valid_period_or_amount_or_rate_positive(data, expected_result):
-    assert _validate_data(data) == expected_result
-
+    
