63080a7f6074909fef5349d576121be661d0b889
fuserwyn <ivansishko@gmail.com>
2022-11-01 22:29:06 +0300

    uppload

--- app/main.py
+++ app/main.py
@@ -9,46 +9,9 @@ from pydantic import BaseModel, ValidationError, validator
  
 app = FastAPI()
 
-class Deposit(BaseModel):
-    date: str
-    periods: int
-    amount: int
-    rate: float
-    
-    @validator('date')
-    def is_str(cls, date):
-        if type(date) != str:
-            raise ValueError("Тип данных для date должен быть str")
-        return date
-    
-    @validator('periods')
-    def is_int_per(cls, periods):
-        if type(periods) != str:
-            raise ValueError("Тип данных для periods должен быть int")
-        return periods
-
-    @validator('amount')
-    def is_int_amount(cls, amount):
-        if type(amount) != str:
-            raise ValueError("Тип данных для amount должен быть int")
-        return amount
-    
-    @validator('rate')
-    def is_float(cls, rate):
-        if type(rate) != str:
-            raise ValueError("Тип данных для rate должен быть float")
-        return rate
-
 class DataValidationError(Exception):
     def __init__(self, msg: str):
         self.msg = msg
-
-@app.exception_handler(DataValidationError)
-def data_validation_exception_handler(request: Request, exc: DataValidationError):
-    return JSONResponse(
-        status_code = 400,
-        content={'error': exc.msg}
-    )
 
 def valid_open_date(date): 
     result = True 
@@ -59,16 +22,63 @@ def valid_open_date(date):
     finally:
         return result
 
-def _validate_data(date, periods, amount, rate):
-    if not valid_open_date(date):
-        raise DataValidationError("Формат даты должен быть dd.mm.YY")
-    if periods > 60 or periods < 1:
-        raise DataValidationError("Количество месяцев по вкладу должно быть от 1 до 60")
-    if amount <= 9999 or amount > 3000000:
-        raise DataValidationError("Сумма вклада должна составлять от 10000 до 3000000") 
-    if rate < 1 or rate > 8:
-        raise DataValidationError("процент по вкладу должен составлять от 1 до 8")
-    return 200
+
+class Deposit(BaseModel):
+    date: str
+    periods: int
+    amount: int
+    rate: float
+    
+    @validator('date')
+    def validate_date_type(cls, date):
+        if not isinstance(date, str):
+            raise TypeError("Тип данных для date должен быть str")
+        return date
+    
+    @validator('periods', 'amount')
+    def validate_integers(cls, value):
+        if not isinstance(value, int):
+            raise TypeError("Тип данных для periods и amount должен быть int")
+        return value
+    
+    @validator('rate')
+    def is_float(cls, rate):
+        if not isinstance(rate, float):
+            raise TypeError("Тип данных для rate должен быть float")
+        return rate
+
+    @validator("periods")
+    def validate_periods_range(cls, periods):
+        if not 1 <= periods <= 60:
+            raise DataValidationError("Значение должно находиться в диапазоне от 1 до 60 включительно")
+        return periods
+    
+    @validator("amount")
+    def validate_amount_range(cls, amount):
+        if amount <= 9999 or amount > 3000000:
+            raise DataValidationError("Сумма вклада должна составлять от 10000 до 3000000") 
+        return amount
+    
+    @validator("rate")
+    def validate_rate_range(cls, rate):
+        if rate < 1 or rate > 8:
+            raise DataValidationError("процент по вкладу должен составлять от 1 до 8")
+        return rate
+    
+    @validator("date")
+    def validate_date_range(cls, date):
+        if not valid_open_date(date):
+            raise DataValidationError("Формат даты должен быть dd.mm.YY")
+        return date
+    
+    
+    
+@app.exception_handler(DataValidationError)
+def data_validation_exception_handler(request: Request, exc: DataValidationError):
+    return JSONResponse(
+        status_code = 400,
+        content={'error': exc.msg}
+    )
 
 @app.get("/")
 def root():
@@ -77,27 +87,26 @@ def root():
 @app.post("/hello")
 def hello(input_json):
     try:
-        parsed_json = Deposit.parse_raw(input_json)
+        parsed_json = json.loads(input_json)
+        deposit = Deposit(**parsed_json)
     except ValidationError as e:
-        raise DataValidationError(e.json())  
-    date = parsed_json.date
-    periods = parsed_json.periods
-    amount = parsed_json.amount
-    rate = parsed_json.rate
-    status = _validate_data(date, periods, amount, rate)
-    if status == 200:
-        date_deposit_open = datetime.strptime(date, "%d.%m.%Y")
-        date_to_json_list = []
-        amount_to_json_list = []
-        curr_date = date_deposit_open
-        for _ in range(periods):
-            amount = amount * (1 + rate/(12 * 100))
-            curr_date += relativedelta(months=1)
-            curr_month_maxdays = calendar.monthrange(curr_date.year, curr_date.month)[1]
-            payment_day = min(curr_month_maxdays, date_deposit_open.day)
-            curr_date = curr_date.replace(day=payment_day)
-            date_to_json_list.append(curr_date.strftime("%d.%m.%Y"))
-            amount_to_json_list.append(round(amount, 2))
-        json_dict_out = dict(zip(date_to_json_list, amount_to_json_list))
-        # json_object_out = json.dumps(json_dict_out) #Если нужна json строка
+        raise DataValidationError(str(e.json()))  
+    date = deposit.date
+    periods = deposit.periods
+    amount = deposit.amount
+    rate = deposit.rate
+    date_deposit_open = datetime.strptime(date, "%d.%m.%Y")
+    date_to_json_list = []
+    amount_to_json_list = []
+    curr_date = date_deposit_open
+    for _ in range(periods):
+        amount = amount * (1 + rate/(12 * 100))
+        curr_date += relativedelta(months=1)
+        curr_month_maxdays = calendar.monthrange(curr_date.year, curr_date.month)[1]
+        payment_day = min(curr_month_maxdays, date_deposit_open.day)
+        curr_date = curr_date.replace(day=payment_day)
+        date_to_json_list.append(curr_date.strftime("%d.%m.%Y"))
+        amount_to_json_list.append(round(amount, 2))
+    json_dict_out = dict(zip(date_to_json_list, amount_to_json_list))
+    # json_object_out = json.dumps(json_dict_out) #Если нужна json строка
     return json_dict_out
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 from urllib import response
 from fastapi.testclient import TestClient
 from app.main import app
-from app.main import DataValidationError, _validate_data, valid_open_date
+from app.main import DataValidationError, valid_open_date
 
 @pytest.mark.parametrize("data, expected_bool", 
     [({
@@ -31,64 +31,6 @@ from app.main import DataValidationError, _validate_data, valid_open_date
 def test_valid_open_date(data, expected_bool):
     assert valid_open_date(data) == expected_bool
 
-@pytest.mark.parametrize("expected_exception, data",
-    [(DataValidationError,{
-        "date": '31.01.2021',
-        "periods": 0, 
-        "amount": 10000, 
-        "rate": 6
-    }), (DataValidationError,{
-        "date": '31.01.2021',
-        "periods": 3, 
-        "amount": 9999, 
-        "rate": 6
-    }),(DataValidationError,{
-        "date": '31.01.2021',
-        "periods": 3, 
-        "amount": 10000, 
-        "rate": 9
-    }),(TypeError,{
-        "date": '31.01.2021',
-        "periods": '3', 
-        "amount": 10000, 
-        "rate": 6
-    }),(TypeError,{
-        "date": '31.01.2021',
-        "periods": 3, 
-        "amount": '10000', 
-        "rate": 6
-    }),(TypeError,{
-        "date": '31.01.2021',
-        "periods": 3, 
-        "amount": 10000, 
-        "rate": '6'
-    })] )
-
-def test_valid_periods_or_amount_or_rate_negative(expected_exception, data):
-    with pytest.raises(expected_exception):
-        _validate_data(data)
-
-@pytest.mark.parametrize("data, expected_result",[({
-        "date": '31.01.2021',
-        "periods": 3, 
-        "amount": 10000, 
-        "rate": 6
-    }, 200), ({
-        "date": '31.12.2022',
-        "periods": 60, 
-        "amount": 3000000, 
-        "rate": 8
-    }, 200),({
-        "date": '19.03.2000',
-        "periods": 30, 
-        "amount": 100000, 
-        "rate": 7.99
-    }, 200)
-    ])
-
-def test_valid_periods_or_amount_or_rate_positive(data, expected_result):
-    assert _validate_data(data) == expected_result
-
 client = TestClient(app)
         
 def test_main_url():
@@ -101,13 +43,8 @@ def test_main_url_bad():
     assert response.status_code == 404
 
 def test_post():
-    deposit = {
-        "date": '31.01.2021',
-        "periods": 3, 
-        "amount": 10000, 
-        "rate": 6
-    }
-    response = client.post('/hello', json = deposit)
+    deposit = '{"date": "31.01.2021","periods": 3, "amount": 10000, "rate": 6}'
+    response = client.post('/hello', deposit)
     assert response.status_code == 200
     assert response.json() == {
                         "28.02.2021": 10050,
